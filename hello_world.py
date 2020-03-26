231e0fc75849afcdd095f7a6c669d38d1add4ad0
Ubuntu <ubuntu@ip-172-31-46-83.ec2.internal>
2020-03-26 19:10:04 +0000

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python 3
+
+# Created by: Andrew Coxall
+# Created on: Mar 26, 2020
+# This is "hello world" but, they way Dad wants it
+
+print("Hello, World!")
+
+def Main():
+    print("Hello world!")
+    
+if __name__=="__main__":
+    main()
+    
